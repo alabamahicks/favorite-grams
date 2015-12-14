70e3219f5a70a6865e1192da37670a304cb7e611
Sarah Hicks <sarah.hicks@emerson.com>
2015-12-14 14:10:50 -0600

    Basic page layout - needs better responsiveness

--- js/app.js
+++ js/app.js
@@ -1,49 +1,17 @@
-//TODO: bug: after clear all, can't add more items
-//TODO: locate entry box in card
-//TODO: add themeing
-//TODO: R2add images
-//TODO: R2add dragging
-//TODO: R2add reorder
-//TODO: R3add local offline storage
+
 
 $( document ).ready(function() {
 
-	//$( document ).on('click', 'li.addedItem', function( event ) {
-	//	console.log(event);
-	//	// SELECT ONE
-	//	selectOne();
-	//
-	//});
-
-	//only original items
-	//$("li.addedItem").each(function () {
-
-	// CHECK ONE - TODO: Bug - requires two clicks, and clicking twice on .addItem selects it
 	$(document).on('click', 'li.addedItem'.each, function (e) {
 		//$(this).click(function (e) {
 			//TOGGLES CHECKED
-			if ($(e.target).hasClass("checked")) {
-				$(e.target).removeClass("checked");
-			} else {
-				//else add it
-				$(e.target).addClass("checked");
-			}
+			//if ($(e.target).hasClass("checked")) {
+			//	$(e.target).removeClass("checked");
+			//} else {
+			//	//else add it
+			//	$(e.target).addClass("checked");
+			//}
 		//});
-	});
-
-	$( document ).on('click', 'div.delete-one', function( event ) {
-		// DELETE ONE - remove parent of div...
-		$(this).closest('.added-item').remove();
-	});
-
-	$( '#addItem').click(function() {
-		//SHOW ENTRY
-		$('.entry-box').show();
-	});
-
-	$( 'header.delete-all').click(function(){
-		console.log('clear all');
-		clearAll();
 	});
 
 });
@@ -54,17 +22,6 @@ $( document ).keydown(function( event ){
 		addOne($('input.entry-box').val());
 	}
 });
-
-//ADD ONE
-function addOne (enteredString) {
-	var liTitle = enteredString;
-	$('<li class="added-item">' + '<p>' + liTitle + '</p>' + '<div class="delete-one">-</div>'+ '</li>').insertBefore("#addItem");
-}
-
-//CLEAR ALL
-function clearAll() {
-	$('#list li:not(:last)').remove();
-}
 
 
 
